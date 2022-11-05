76d4191978d10ea51fe2c616654b028af3a36197
Abhay Tomar <77535285+abhaytomar2002@users.noreply.github.com>
2022-11-06 00:05:58 +0530

    Add user choice page

--- Frontend/src/Pages/FindOrWant.jsx
+++ Frontend/src/Pages/FindOrWant.jsx
@@ -1,3 +1,4 @@
+import { requirePropFactory } from '@mui/material';
 import React from 'react'
 import NavbarHarshit from '../Components/Home/NavbarHarshit';
 import "../styles/findorwant.css";
@@ -6,26 +7,64 @@ import "../styles/findorwant.css";
 const FindOrWant = () => {
   return (
     <>
-
     <NavbarHarshit/>
+    <div className='card-container left'>
+      <div className='image-container'>
+        <img src={require("../assets/volunteer_image_findorwant.webp")}
+                alt="Image of Dog" />
+      </div>
+      <div className='card-title'>
+        <h3>abc</h3>
+      </div>
+      <div className='card-body'>
+        <div className='card-title'>
+          <h3>abc</h3>
+        </div>
+        <div className='card-body'>
+          <p>xyz</p>
+        </div>
+      </div>
+      <div className='btn'>
+        <button>
+          <a >
+            view more
+          </a>
+        </button>
+      </div>
+    </div>
+    <div className='card-container right'>
+      <div className='image-container'>
+        <img src={require("../assets/ngo_image_findorwant.webp")}
+                alt="Image of Dog" />
+      </div>
+      <div className='card-title'>
+        <h3>pqr</h3>
+      </div>
+      <div className='card-body'>
+        <div className='card-title'>
+          <h3>xyz</h3>
+        </div>
+        <div className='card-body'>
+          <p>zbc</p>
+        </div>
+      </div>
+      <div className='btn'>
+        <button>
+          <a >
+            view more
+          </a>
+        </button>
+      </div>
+    </div>
 
-    <button class="split left">
-  <div class="centered">
-  {/* can add image here */}
-    <h1>WANT TO VOLUNTEER</h1>
-    <p>NGO's, Organisations, and .... bakwas</p>
-  </div>
-</button>
-
-<button class="split right">
-  <div class="centered">
-  {/* img krlenge  */}
-    <h2>FIND A VOLUNTEER</h2>
-    <p>For those who want to contribute .... bakwas </p>
-  </div>
-</button>
+   
 </>
   )
 }
 
 export default FindOrWant
+/*
+<button class="split left">
+<div class="centered">
+{/* can add image here */
+ 
